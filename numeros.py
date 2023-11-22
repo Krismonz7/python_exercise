212d0f2aaea4dd4d9ec1748c75bf00bb536a2d31
Krismonz7 <kriz.monz707@gmail.com>
2023-11-22 15:44:32 -0500

    ejercicio 1 tuplas - seccion 6

--- numeros.py
+++ numeros.py
@@ -1,10 +1,9 @@
-# Programa para almacenar la suma de todos los numeros del 1 al 100
-rango =  list(range(1,100,1))
-suma = 0
-for numero in rango:
-
-    suma += numero
-    if numero%10 == 0:
-        print(numero)
-
-print("La suma total es de : ",suma)
+# no borres esta línea
+tupla = (1,2,3,4,5,6,7,8,9,10,10,12,13,14,15)
+# completa el ejercicio
+print(tupla[-1])
+print(len(tupla))
+print(tupla.index(123))
+print(tupla[0:3])
+print(tupla[4])
+print(tupla.count(4))
