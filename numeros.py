e45497dc78872139f98f0127371bb8900657ecc5
Krismonz7 <kriz.monz707@gmail.com>
2023-11-22 16:54:11 -0500

    ejercicio 2 conjuntos- seccion 6

--- numeros.py
+++ numeros.py
@@ -1,9 +1,10 @@
-# no borres esta línea
-tupla = (1,2,3,4,5,6,7,8,9,10,10,12,13,14,15)
-# completa el ejercicio
-print(tupla[-1])
-print(len(tupla))
-print(tupla.index(123))
-print(tupla[0:3])
-print(tupla[4])
-print(tupla.count(4))
+# Variables del ejercicio (no las modifiques)
+grupo = {"Miguel", "Blanca", "Mario", "Andrés"}
+
+# Completa el ejercicio
+grupo.update(["Ana","Ramón","Marta","Eric","David"])
+
+grupo.remove("Mario")
+grupo.remove("Miguel")
+grupo.remove("Ramón")
+print(grupo)
