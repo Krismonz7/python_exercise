b0b2e77e1023533ec9569a132e56c08c9e49146b
Krismonz7 <kriz.monz707@gmail.com>
2023-11-24 16:55:38 -0500

    clase 91 -  ejercicios optativos 1

--- escribir_lineas.py
+++ escribir_lineas.py
@@ -1,12 +1,17 @@
-#clase 90 - f-string:
-nombre = "Kirby"
-cadena = "Hola {}".format(nombre)
-print(cadena)
-cadena = f"Hola {nombre}"
+#Clase 91 :ejercicios optativos 1
+#Hola mundo alineado a la derecha 10caracteres
+print("{:>20}".format("Hola mundo"))
+#Hola mundo truncado
+print("{:.3}".format("Hola mundo"))
 
-print(cadena)
-#Formateos avanzados
-n1 = 189.30
-n2 = 3.098
-print(f"{n2:7.3f}")
-print(f"{n2:7.3f}")
+#Hola mundo alineado en 20 espacios con trucamiento
+print("{:^40.3}".format("Hola mundo"))
+#Formatea los soguientes numeros y rellenalso con la mismacantidad de ceros
+n1 = 400.03
+n2 = 3.098755
+n3 = 14.174
+#Eespacios.cantidad de decimalesf
+print("{:08.2f}".format(n1))
+print("{:08.2f}".format(n2))
+
+print("{:08.2f}".format(n3))                                     
